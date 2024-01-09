c4bf5f61b68109927cd69ce19380f79ac1bc9d6f
Michael Nordmeyer <michaelnordmeyer@users.noreply.github.com>
2024-01-09 23:27:36 +0100

    Default to feeds with 10 posts

--- benchmark.sh
+++ benchmark.sh
@@ -6,7 +6,7 @@ for site in hugo gh-pages jekyll; do
   for payload in lorem htmltest; do
     for number in 100 1000 10000 100000; do
       cd ${site}
-      echo "==> Running benchmark for ${site} with ${payload} for ${number} times..."
+      echo && echo  "==> Running benchmark for ${site} with ${payload} for ${number} times..."
       [[ ${site} = "hugo" ]] && engine="hugo" || engine="jekyll"
       ../generate-posts.sh ${engine} ${payload} ${number}
       if [ ${site} = "hugo" ]; then
@@ -16,7 +16,7 @@ for site in hugo gh-pages jekyll; do
         rm -rf public resources content
       else
         for i in {1..5}; do
-          rm -rf _site .jekyll-cache && /usr/bin/time -h bundle exec jekyll build --quiet
+          rm -rf _site && /usr/bin/time -h bundle exec jekyll build --quiet
         done
         rm -rf _site .jekyll-cache _posts about.md
       fi
